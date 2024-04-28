425dcec41bbbff53e0b1ec6eeb7e3bb458576cf7
prashant123singh <prashant123singh@outlook.com>
2024-04-28 08:15:03 -0500

    inferencing and webapp

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prashant',
+    author_email= 'prashant123singh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
